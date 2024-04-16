be8606afdc5ff3e64148c2272530b344b64a36d8
Justin Egbufoama <jegbufoama@gmail.com>
2024-04-16 13:05:32 -0400

    Add message to created index file in html directory - WIP

--- apache.sh
+++ apache.sh
@@ -6,3 +6,6 @@ sudo yum -y install httpd
 sudo yum systemctl enable httpd
 
 sudo yum systemctl start httpd
+
+# Add message to created index file in html directory
+echo "Yeah I did that!!" >> /var/www/html/index.html
