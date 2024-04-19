2ca9d210c2ff957a203d996de31d108dc43b2b5d
Jegbu <150167799+Jegbu@users.noreply.github.com>
2024-04-18 20:29:37 -0400

    add security group - WIP

--- 3 tier vpc-main.tf
+++ 3 tier vpc-main.tf
@@ -62,8 +62,6 @@ resource "aws_subnet" "private_subnet_az_2" {
   }
 }
 
-
-
 # Route Table creation
 resource "aws_route_table" "Jegbu" {
   vpc_id = aws_vpc.jegbu_vpc.id
@@ -88,6 +86,48 @@ resource "aws_route_table_association" "public_subnet_az_2" {
   subnet_id      = aws_subnet.public_subnet_az_2.id
   route_table_id = aws_route_table.Jegbu.id
 }
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_security_group" "example" {
+  name        = "example"
+  description = "Example security group"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "example"
+  }
+}
+
 
 
 
