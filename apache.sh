262de6bbb7f46e2905342cae7c43dadd0d404942
Jegbu <150167799+Jegbu@users.noreply.github.com>
2024-04-19 12:09:38 -0400

    Changed index message

--- apache.sh
+++ apache.sh
@@ -8,4 +8,4 @@ sudo systemctl enable httpd
 sudo systemctl start httpd
 
 # Add message to created index file in html directory
-sudo bash -c 'echo "Yeah I Justin did that!!!!!" >> /var/www/html/index.html'
+sudo bash -c 'echo "To err is human, but to really foul things up you need a computer. - Paul R. Ehrlich" >> /var/www/html/index.html'
