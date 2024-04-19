e2510875f045bc78c57059776ba8636fc3e882d9
Jegbu <150167799+Jegbu@users.noreply.github.com>
2024-04-18 20:26:48 -0400

    Deleted NACL for public subnet 2 - WIP

--- 3 tier vpc-main.tf
+++ 3 tier vpc-main.tf
@@ -63,32 +63,6 @@ resource "aws_subnet" "private_subnet_az_2" {
 }
 
 
-#NACL for public subnet AZ 2
-resource "aws_network_acl" "NACL_2" {
-  vpc_id = aws_vpc.jegbu_vpc.id
-
-  egress {
-    protocol   = "tcp"
-    rule_no    = 200
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 443
-    to_port    = 443
-  }
-
-  ingress {
-    protocol   = "tcp"
-    rule_no    = 100
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 80
-    to_port    = 80
-  }
-
-  tags = {
-    Name = "AZ 2 NACL"
-  }
-}
 
 #NACL for Private Subnet AZ 1
 resource "aws_network_acl" "NACL_private_1" {
