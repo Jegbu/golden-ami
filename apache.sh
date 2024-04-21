eef44126247f926821664ebedd27fba8927f7002
Justin Egbufoama <jegbufoama@gmail.com>
2024-04-21 12:15:51 -0400

    amazon linux

--- apache.sh
+++ apache.sh
@@ -1,15 +1,11 @@
-# This is for ubuntu servers
-sudo apt-get update # update server(s)
+sudo yum update # update server(s)
 
 # Install Apache
-sudo apt-get -f install
+sudo yum -y install httpd
 
-sudo apt-get install -y apache2 apache2-bin media-types
+sudo systemctl enable httpd
 
-sudo systemctl enable apache2
-
-sudo systemctl start apache2
+sudo systemctl start httpd
 
 # Add message to created index file in html directory
-sudo bash -c 'echo \"To err is human, but to really foul things up you need a computer. - Paul R. Ehrlich\" >> /var/www/html/index.html'
-
+sudo bash -c 'echo "To err is human, but to really foul things up you need a computer. - Paul R. Ehrlich" >> /var/www/html/index.html'
