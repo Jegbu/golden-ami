8ba41e479824708449197ea3a0ea6617547f3eb2
Justin Egbufoama <jegbufoama@gmail.com>
2024-04-23 17:19:39 -0400

    WIP

--- as.sh
+++ as.sh
@@ -9,4 +9,4 @@ terraform apply -auto-approve
 # Packer
 echo "Running Packer build! Please be patient!"
 cd packer # Change directory to packer
-packer build -var 'clean_resource_name=ami-0ce315e06ff440e6c' ami.json
+packer build -var 'clean_resource_name=amazon-linux-ami' ami.json
