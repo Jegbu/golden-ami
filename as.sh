1ff061e5670097b2b5bdf69dad447c759dee8877
Justin Egbufoama <jegbufoama@gmail.com>
2024-04-29 00:34:32 -0400

    Automation script

--- as.sh
+++ as.sh
@@ -1,7 +1,7 @@
-#! /bin/bash
+#!/bin/bash
 
-#Terraform
-echo "Initalizing Terraform"
+# Terraform
+echo "Initializing Terraform"
 terraform init
 echo "Applying Terraform configuration"
 terraform apply -auto-approve
@@ -14,7 +14,7 @@ else
     exit 1
 fi
 
-#Retrieve security group ID from Terraform output
+# Retrieve security group ID from Terraform output
 security_group_id=$(terraform output -raw security_group_id)
 
 # Check if security group ID is empty
@@ -24,7 +24,6 @@ if [ -z "$security_group_id" ]; then
 fi
 
 echo "Security group ID retrieved: $security_group_id"
-
 
 # Fetch VPC ID using AWS CLI
 echo "Fetching VPC ID for jegbu_vpc"
@@ -52,28 +51,27 @@ echo "Subnet ID for Public Subnet AZ1 in jegbu_vpc retrieved: $subnet_id"
 
 # Packer
 echo "Running Packer build! Please be patient!"
-cd packer # Change directory to packer
+cd packer || exit # Change directory to packer
+
+# Build the AMI with Packer
 build_output=$(packer build -machine-readable ami.json)
 
 # Debug statement to print Packer build output
 echo "Packer build output: $build_output"
 
-# Pass security group ID as a variable to Packer
-packer build -var "security_group_id=$security_group_id" -var 'clean_resource_name=amazon-linux-ami' ami.json
-
+# Pass security group ID and subnet ID as variables to Packer
+packer build -var "security_group_id=$security_group_id" -var "subnet_id=$subnet_id" -var 'clean_resource_name=amazon-linux-ami' ami.json
 
 # Check if Packer build was successful
 if [ $? -eq 0 ]; then
     echo "Packer build was successful. Proceeding to create EC2 instance."
 else
     echo "Error: Packer build has failed. Exiting script."
-    exit 1    
+    exit 1
 fi
 
 # Retrieve AMI ID from Packer's output
 ami_id=$(echo "$build_output" | grep -oE 'ami-[a-zA-Z0-9]{17}')
-
-
 
 echo "AMI ID after retrieval: $ami_id"
 
@@ -85,22 +83,13 @@ fi
 
 echo "AMI ID created: $ami_id"
 
-# Use AMI ID for ec2 instance
-aws ec2 run-instances --image-id $ami_id --count 1 --instance-type t2.micro --key-name myec2key --security-group-ids $security_group_id
+# Use AMI ID for EC2 instance creation
+aws ec2 run-instances --image-id "$ami_id" --count 1 --instance-type t2.micro --key-name myec2key --security-group-ids "$security_group_id" --subnet-id "$subnet_id"
 
-# Check if ec2 instance was good or not
-# Check if instance creation was successful
+# Check if EC2 instance creation was successful
 if [ $? -eq 0 ]; then
     echo "EC2 instance created successfully! Good to go! :)"
 else
     echo "Error: EC2 instance creation failed! :("
     exit 1
 fi
-# Check if Packer build was successful
-#if [ $? -eq 0 ]; then 
-#    cd ..
-#    terraform destroy -auto-approve
-# else
-#    echo "Error: Packer build has failed. :( Terraform resources will not be destroyed."
-#    exit 1
-# fi    
