84fb8b938305d2c2e6f24bde987f19cd3565a53e
Justin Egbufoama <jegbufoama@gmail.com>
2024-04-29 19:02:17 -0400

    Automation script

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,3 +1,10 @@
+data "aws_subnet" "this" {
+  filter {
+    name   = "tag:Name"
+    values = ["Public Subnet AZ1"]
+  }
+}
+
 data "aws_ami" "custom" {
   most_recent = true
 
@@ -17,7 +24,7 @@ data "aws_ami" "custom" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.custom.id
   instance_type = "t2.micro"
-
+  subnet_id     = data.aws_subnet.this.id
   tags = {
     Name = "apache"
   }
